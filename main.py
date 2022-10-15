b3b53e4f208084f3caf76da8df17834dfe6cde22
guilherme-mota <motaguilherme99@ua.pt>
2022-10-15 14:04:04 +0100

    deteção da face e desenho da boundingbox

--- main.py
+++ main.py
@@ -10,6 +10,7 @@
 # ------------------------
 # Imports
 # ------------------------
+from copy import deepcopy
 import cv2
 import numpy as np
 
@@ -29,18 +30,34 @@ def main():
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(window_name, 800, 500)
 
+    # Load Pre-trained Classifiers
+    face_detector = cv2.CascadeClassifier('/home/guilherme/workingcopy/opencv-4.5.4/data/haarcascades/haarcascade_frontalface_default.xml')
+
 
     # ------------------------
     # Execution
     # ------------------------
     while capture.isOpened():  # loop through all frames
         ret, image_original = capture.read()  # get a frame, ret will be true or false if getting succeeds
+        image_gray = cv2.cvtColor(image_original, cv2.COLOR_BGR2GRAY)  # convert color image to gray
+        image_gui = deepcopy(image_original)
 
         if ret == False:
             break
 
+        # ------------------------------------------
+        # Detection of faces
+        # ------------------------------------------
+        bboxes = face_detector.detectMultiScale(image_gray, 1.1, 3, 0, (0, 0), (0, 0));
+        print(bboxes)
+
+        # Draw bbox around faces
+        for bbox in bboxes:
+            x1, y1, w, h = bbox
+            cv2.rectangle(image_gui, (x1, y1), (x1+w, y1+h), (255, 0, 0), 3)
+
         # Display Image Capture
-        cv2.imshow(window_name, image_original)
+        cv2.imshow(window_name, image_gui)
 
         if cv2.waitKey(1) == ord('q'):
             break
